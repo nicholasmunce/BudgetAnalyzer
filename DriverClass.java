b4817b3b5ef08887dfef94329d5c4754d4bee47f
nicholasmunce <nrobert.21@gmail.com>
2016-05-28 07:30:07 -0500

    Add files via upload
    This update makes significant changes to the overall framework of the application. 
    
    1.) Adds file output option to either write the data in a .txt file or a .xls file
    2.) Accesses files via the internet, no longer a need to hold the .txt files on your local drive.
    3.) Consolidation of all background processing into one class, all menu interaction into another.
    4.) Adds some limited exception handling, will be expanded when I get some more time.
    
    *note* : two spreadsheets, attached here, must be stored in the project folder, in a folder called Data in order for processing to work correctly.

--- DriverClass.java
+++ DriverClass.java
@@ -1,36 +1,36 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DriverClass {
 
+	public static int outputChoice;
+	
 	public static void main(String [] args) throws IOException{
-		CensusDataReader inputYear = new CensusDataReader();
-		RawDataByYear getData = new RawDataByYear();
+		BehindTheScenes operator = new BehindTheScenes();
 		Scanner systemIn = new Scanner(System.in);
 		
 		//read in code data from spreadsheet
-		String [][] localArray = inputYear.getExcelInfo("data/itemcodes.xls");
+		String [][] localArray = operator.getFinancialInfo("data/itemcodes.xls");
+		System.out.println("How would you like your data to be output?");
+		System.out.println("1.) In a .txt file");
+		System.out.println("2.) In a .xls spreadsheet");
+		outputChoice = Integer.parseInt((systemIn.nextLine()));
+		System.out.println("1.) Specific year, Specific State");
+		System.out.println("2.) All years, Specific State");
+		String menuChoice = systemIn.nextLine();
 		
-		//get user data
-		System.out.println("Enter a year to read: ");
-		String input = systemIn.nextLine();
-		String inputProcessed = "data/" +input + ".txt";
-		System.out.println("Enter a filename to save to: ");
-		String output = "data/" + systemIn.nextLine() + ".txt";
-		System.out.println("Enter the issue that you are interested in:");
-		String search = systemIn.nextLine();
-		
-		//take user info and match it to raw data file information
-		double totalMoney = 0;
-		ArrayList <String> finalArray = getData.getFinancialInfo(getData.matchToCodes(inputYear.getCodes(search, localArray), getData.getStateInfo(inputProcessed)));
-		for(int i = 0; i <finalArray.size(); i++){
-			totalMoney += Double.parseDouble(finalArray.get(i));
-		}
-		
-		totalMoney = totalMoney * 1000;
-		System.out.println("In the year " + input + " the state of Wisconsin spent $" + totalMoney + " on " + search);
-	}
+		switch(menuChoice){
+		case "1":
+			operator.processOption1(localArray, outputChoice);
+			break;
 	
+		
+		case "2":
+			operator.processOption2(localArray, outputChoice);
+			break;
+			
+		}
+	}
 }
+	
+
